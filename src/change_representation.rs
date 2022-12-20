e8f4a709b6b5c6dc2cf94360b49c4e67a00475fb
milan <milan.o.e01@gmail.com>
2022-12-20 11:46:13 +0100

    Change Representation - optional fields

--- src/change_representation.rs
+++ src/change_representation.rs
@@ -4,10 +4,15 @@ use std::collections::HashMap;
 #[derive(Serialize, Deserialize)]
 pub struct ChangeRepresentation {
     pub actions: Vec<Action>,
+    #[serde(default)]
     pub before: HashMap<String, serde_json::Value>,
+    #[serde(default)]
     pub after: HashMap<String, serde_json::Value>,
+    #[serde(default)]
     pub after_unknown: HashMap<String, serde_json::Value>,
+    #[serde(default)]
     pub before_sensitive: HashMap<String, serde_json::Value>,
+    #[serde(default)]
     pub after_sensitive: HashMap<String, serde_json::Value>,
     pub replace_paths: Option<Vec<Vec<String>>>,
 }
